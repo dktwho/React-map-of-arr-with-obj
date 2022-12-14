2ac052f450d7cc71fdcee3e5a90b45b0c32c97ac
Alexander Korobov <dktwho@mail.ru>
2022-12-14 16:12:42 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const prods = [
+    {name: 'product1', cost: 100},
+    {name: 'product2', cost: 200},
+    {name: 'product3', cost: 300},
+  ];
+
+  const result = prods.map((elem, index) => {
+    return <p key={index}>
+      <span>{elem.name}</span>
+      <span> {elem.cost}</span>  
+       </p>
+  })
+
+  const users = [
+    {name: 'user1', surn: 'surn1', age: 30},
+    {name: 'user2', surn: 'surn2', age: 31},
+    {name: 'user3', surn: 'surn3', age: 32},
+  ];
+
+  const result2 = users.map((item, idx) => {
+    return <li key={idx}>{item.name} - {item.surn} - {item.age}</li>
+  })
+
+  const prods2 = [
+    {id: 1, name: 'product1', cost: 100},
+    {id: 2, name: 'product2', cost: 200},
+    {id: 3, name: 'product3', cost: 300},
+  ];
+
+  const result3 = prods2.map(prod => {
+    return <li key={prod.id}>{prod.name} - {prod.cost}</li>
+  })
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>{result}</ul>
+      <br />
+      <hr />
+      <ul>{result2}</ul>
+      <br />
+      <hr />
+      <ul>{result3}</ul>
+
     </div>
   );
 }
